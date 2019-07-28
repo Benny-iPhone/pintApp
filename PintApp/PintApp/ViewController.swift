9a85f08a748a51f8ddfaadf0b10351378d27523d
BennyDavidovitz <>
2019-07-28 18:05:08 +0300

    Create Xcode Proj

---
+++ PintApp/PintApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PintApp
+//
+//  Created by Benny Davidovitz on 28/07/2019.
+//  Copyright © 2019 HackerU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
